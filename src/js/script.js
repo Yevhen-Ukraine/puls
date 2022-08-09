ef3ba09c9ae13c6c66eb998c13f153d2bb12e410
Yevhen-Ukrain <tishenko130180@gmail.com>
2022-08-09 22:31:23 +0300

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -79,5 +79,25 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+3 (999) 999-99-99");
 
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 }); 
           
